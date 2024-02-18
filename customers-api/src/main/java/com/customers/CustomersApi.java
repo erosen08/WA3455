f466fdb452a84868be21389372d18086d5092a49
Ethan <erosenberg39@gmail.com>
2024-02-18 04:13:10 +0000

    main setup

---
+++ customers-api/src/main/java/com/customers/CustomersApi.java
@@ -0,0 +1,13 @@
+package com.customers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomersApi {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomersApi.class, args);
+	}
+
+}
